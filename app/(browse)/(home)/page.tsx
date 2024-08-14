0081ff78a68d74fcdc066ff9b66acf3a3d73761c
Vínicius Santos <viniciuszero@live.com>
2024-08-14 15:59:29 -0300

    Integrating with web hooks | creation of navbar and home sidebar

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,10 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
     <div className="flex flex-col gap-y-4">
       <h1>dashboard</h1>
-      <UserButton />
     </div>
   );
 }
